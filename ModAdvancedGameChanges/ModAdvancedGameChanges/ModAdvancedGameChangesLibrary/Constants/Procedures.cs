5adaacfe04d58bbbc60690fc8f67ef650969c3c1
georgius <juraj.horvath.mail@gmail.com>
2025-10-25 23:11:51 +0200

    added procedures

--- ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/Procedures.cs
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/Procedures.cs
@@ -5,6 +5,8 @@
         public static class Vanilla
         {
             public const string StaffTraining = "DLC_ADMIN_STAFF_TRAINING";
+
+            public const string StaffFreeTime = "CONTROL_PROCEDURE_NURSE_FREE_TIME";
         }
     }
 }
