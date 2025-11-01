3c069a84fcf3424c0cbf852e08e1cd9b4da664fc
georgius <juraj.horvath.mail@gmail.com>
2025-11-01 13:39:56 +0100

    fixed hiring technologists to training department

--- ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/HiringPanelControllerPatch.cs
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/HiringPanelControllerPatch.cs
@@ -12,25 +12,46 @@ namespace ModAdvancedGameChanges
         [HarmonyPatch(typeof(HiringPanelController), nameof(HiringPanelController.SortTechnologistButtons))]
         public static bool SortTechnologistButtonsPrefix(bool clinic, HiringPanelController __instance)
         {
-            if ((!ViewSettingsPatch.m_enabled) || (!ViewSettingsPatch.m_labEmployeeBiochemistry[SettingsManager.Instance.m_viewSettings].m_value))
+            if (!ViewSettingsPatch.m_enabled)
             {
                 // Allow original method to run
                 return true;
             }
 
-            GameDBDepartment departmentType = Hospital.Instance.m_activeDepartment.GetEntity().GetDepartmentType();
-
-            if (departmentType == Database.Instance.GetEntry<GameDBDepartment>(Departments.Vanilla.MedicalLaboratories))
+            if (ViewSettingsPatch.m_labEmployeeBiochemistry[SettingsManager.Instance.m_viewSettings].m_value)
             {
-                for (int i = 0; i < __instance.m_technologistCharacters.Count; i++)
+                GameDBDepartment departmentType = Hospital.Instance.m_activeDepartment.GetEntity().GetDepartmentType();
+
+                if (departmentType == Database.Instance.GetEntry<GameDBDepartment>(Departments.Vanilla.MedicalLaboratories))
                 {
-                    __instance.m_technologistCharacters[i].SetActive(false);
+                    for (int i = 0; i < __instance.m_technologistCharacters.Count; i++)
+                    {
+                        __instance.m_technologistCharacters[i].SetActive(false);
+                    }
+
+                    __instance.m_technologistCharacters[1].SetActive(true);
+                    __instance.m_technologistCharacters[1].GetComponent<RectTransform>().anchoredPosition = new Vector3(0f, 0f, 0f);
+
+                    return false;
                 }
+            }
 
-                __instance.m_technologistCharacters[1].SetActive(true);
-                __instance.m_technologistCharacters[1].GetComponent<RectTransform>().anchoredPosition = new Vector3(0f, 0f, 0f);
+            if (ViewSettingsPatch.m_enabledTrainingDepartment)
+            {
+                GameDBDepartment departmentType = Hospital.Instance.m_activeDepartment.GetEntity().GetDepartmentType();
 
-                return false;
+                if (departmentType == Database.Instance.GetEntry<GameDBDepartment>(Departments.Mod.TrainingDepartment))
+                {
+                    for (int i = 0; i < __instance.m_technologistCharacters.Count; i++)
+                    {
+                        __instance.m_technologistCharacters[i].SetActive(false);
+                    }
+
+                    __instance.m_technologistCharacters[0].SetActive(true);
+                    __instance.m_technologistCharacters[0].GetComponent<RectTransform>().anchoredPosition = new Vector3(0f, 0f, 0f);
+
+                    return false;
+                }
             }
 
             return true;
--- ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/LogisticsWorkspacePanelControllerPatch.cs
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/LogisticsWorkspacePanelControllerPatch.cs
@@ -14,37 +14,89 @@ namespace ModAdvancedGameChanges
         [HarmonyPatch(typeof(LogisticsWorkspacePanelController), "OpenHiringCard")]
         public static bool OpenHiringCardPrefix(Shift shift, LogisticsWorkspacePanelController __instance)
         {
-            if ((!ViewSettingsPatch.m_enabled) || (!ViewSettingsPatch.m_labEmployeeBiochemistry[SettingsManager.Instance.m_viewSettings].m_value))
+            if (!ViewSettingsPatch.m_enabled)
             {
                 // Allow original method to run
                 return true;
             }
-            
-            if ((Hospital.Instance.m_activeDepartment.GetEntity().GetDepartmentType() == Database.Instance.GetEntry<GameDBDepartment>(Departments.Vanilla.MedicalLaboratories))
-                && (__instance.m_characterType == DisplayedCharacterType.LAB_SPECIALIST))
+
+            switch (__instance.m_characterType)
             {
-                HiringManager.Instance.m_workspace = LogisticsWorkspacePanelControllerPatch.GetWorkspaceMod(__instance);
+                case DisplayedCharacterType.DOCTOR:
+                    break;
+                case DisplayedCharacterType.NURSE:
+                    break;
+                case DisplayedCharacterType.LAB_SPECIALIST:
+                    {
+                        if (ViewSettingsPatch.m_labEmployeeBiochemistry[SettingsManager.Instance.m_viewSettings].m_value && (Hospital.Instance.m_activeDepartment.GetEntity().GetDepartmentType() == Database.Instance.GetEntry<GameDBDepartment>(Departments.Vanilla.MedicalLaboratories)))
+                        {
+                            HiringManager.Instance.m_workspace = LogisticsWorkspacePanelControllerPatch.GetWorkspaceMod(__instance);
 
-                MapEditorUIController.Instance.m_hiringPanel.transform.parent.gameObject.SetActive(true);
-                MapEditorUIController.Instance.m_hiringPanel.GetComponent<HiringPanelController>().UpdateShift(shift, true);
-                MapEditorUIController.Instance.m_hiringPanel.SetActive(true);
-                MapEditorUIController.Instance.m_hiringPanel.GetComponent<HiringPanelController>().SortHiringSpecializationsButtons(true);
+                            MapEditorUIController.Instance.m_hiringPanel.transform.parent.gameObject.SetActive(true);
+                            MapEditorUIController.Instance.m_hiringPanel.GetComponent<HiringPanelController>().UpdateShift(shift, true);
+                            MapEditorUIController.Instance.m_hiringPanel.SetActive(true);
+                            MapEditorUIController.Instance.m_hiringPanel.GetComponent<HiringPanelController>().SortHiringSpecializationsButtons(true);
 
-                MapEditorUIController.Instance.m_hiringPanel.GetComponent<HiringPanelController>().SetCharacterType(
-                    LopitalTypes.CharacterAdvancedBiochemist,
-                    Icons.ICON_BIOCHEMISTRY_MANAGEMENT - 4, 
-                    StringTable.GetInstance().GetLocalizedText(Skills.Vanilla.SKILL_LAB_SPECIALIST_SPEC_BIOCHEMISTRY, new string[0]));
+                            MapEditorUIController.Instance.m_hiringPanel.GetComponent<HiringPanelController>().SetCharacterType(
+                                LopitalTypes.CharacterAdvancedBiochemist,
+                                Icons.ICON_BIOCHEMISTRY_MANAGEMENT - 4,
+                                StringTable.GetInstance().GetLocalizedText(Skills.Vanilla.SKILL_LAB_SPECIALIST_SPEC_BIOCHEMISTRY, new string[0]));
 
-                __instance.SetEmployeeNumbers(shift, true);
-                UISoundManager.sm_instance.PlaySoundEvent("SFX_UI_WINDOW_OPEN", 1f);
-                MapEditorUIController.Instance.m_hiringPanel.GetComponent<HiringPanelController>().Update();
-                GlobalEventManager.Instance.OnGlobalEvent(new GlobalEvent
-                {
-                    m_eventType = GlobalEventType.GAME_MODE_HIRING_CARD_OPENED
-                });
-                UIManager.FixMacTexts();
+                            __instance.SetEmployeeNumbers(shift, true);
+                            UISoundManager.sm_instance.PlaySoundEvent("SFX_UI_WINDOW_OPEN", 1f);
+                            MapEditorUIController.Instance.m_hiringPanel.GetComponent<HiringPanelController>().Update();
+                            GlobalEventManager.Instance.OnGlobalEvent(new GlobalEvent
+                            {
+                                m_eventType = GlobalEventType.GAME_MODE_HIRING_CARD_OPENED
+                            });
+                            UIManager.FixMacTexts();
 
-                return false;
+                            return false;
+                        }
+
+                        if (ViewSettingsPatch.m_enabledTrainingDepartment && (Hospital.Instance.m_activeDepartment.GetEntity().GetDepartmentType() == Database.Instance.GetEntry<GameDBDepartment>(Departments.Mod.TrainingDepartment)))
+                        {
+                            HiringManager.Instance.m_workspace = LogisticsWorkspacePanelControllerPatch.GetWorkspaceMod(__instance);
+
+                            MapEditorUIController.Instance.m_hiringPanel.transform.parent.gameObject.SetActive(true);
+                            MapEditorUIController.Instance.m_hiringPanel.GetComponent<HiringPanelController>().UpdateShift(shift, true);
+                            MapEditorUIController.Instance.m_hiringPanel.SetActive(true);
+                            MapEditorUIController.Instance.m_hiringPanel.GetComponent<HiringPanelController>().SortHiringSpecializationsButtons(true);
+
+                            MapEditorUIController.Instance.m_hiringPanel.GetComponent<HiringPanelController>().SetCharacterType(
+                                LopitalTypes.CharacterTechnologist,
+                                IconManager.ICON_TECHNOLOGIST_MANAGEMENT - 4,
+                                StringTable.GetInstance().GetLocalizedText(Localizations.Vanilla.LAB_TECHNOLOGIST, new string[0]));
+
+                            __instance.SetEmployeeNumbers(shift, true);
+                            UISoundManager.sm_instance.PlaySoundEvent("SFX_UI_WINDOW_OPEN", 1f);
+                            MapEditorUIController.Instance.m_hiringPanel.GetComponent<HiringPanelController>().Update();
+                            GlobalEventManager.Instance.OnGlobalEvent(new GlobalEvent
+                            {
+                                m_eventType = GlobalEventType.GAME_MODE_HIRING_CARD_OPENED
+                            });
+                            UIManager.FixMacTexts();
+
+                            return false;
+                        }
+                    }
+                    break;
+                case DisplayedCharacterType.JANITOR:
+                    break;
+                //case DisplayedCharacterType.PARAMEDIC:
+                //    break;
+                //case DisplayedCharacterType.DOCTORS_NURSES:
+                //    break;
+                //case DisplayedCharacterType.DOCTOR_CLINIC:
+                //    break;
+                //case DisplayedCharacterType.DOCTOR_HOSPITALIZATION:
+                //    break;
+                //case DisplayedCharacterType.PATIENT:
+                //    break;
+                //case DisplayedCharacterType.ALL_EMPLOYEES:
+                //    break;
+                default:
+                    break;
             }
 
             return true;
