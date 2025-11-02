c1e187801805c922c3a25b03ad6f75ee31ca9d11
georgius <juraj.horvath.mail@gmail.com>
2025-11-02 20:31:08 +0100

    added employee roles and room types

---
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/EmployeeRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace ModAdvancedGameChanges.Constants
+{
+    public static class EmployeeRoles
+    {
+        public static class Vanilla
+        {
+            public const string Receptionist = "EMPL_ROLE_RECEPTIONIST";
+        }
+    }
+}
--- ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/RoomTypes.cs
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/RoomTypes.cs
@@ -10,6 +10,7 @@ namespace ModAdvancedGameChanges.Constants
         public static class Vanilla
         {
             public const string CommonRoom = "ROOM_TYPE_COMMON_ROOM";
+            public const string Reception = "ROOM_TYPE_RECEPTION";
         }
     }
 }
