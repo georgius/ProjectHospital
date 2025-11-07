2d24bc3113740946d6b9907466b0344dd0e6057e
georgius <juraj.horvath.mail@gmail.com>
2025-11-07 17:57:37 +0100

    added needs

--- ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/Needs.cs
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/Needs.cs
@@ -4,11 +4,14 @@
     {
         public static class Vanilla
         {
+            public const string Bladder = "NEED_BLADDER";
+            public const string Boredom = "NEED_BOREDOM";
+            public const string Discomfort = "NEED_DISCOMFORT";
+            public const string HungerPatient = "NEED_HUNGER_PATIENT";
+            public const string HungerStaff = "NEED_HUNGER_STAFF";
             public const string Rest = "NEED_REST";
+            public const string Satisfaction = "NEED_SATISFACTION";
         }
-
-        public const float NeedThreshold = 50f;
-        public const float NeedThresholdCritical = 80f;
 
         public const float NeedMinimum = 0f;
         public const float NeedMaximum = 100f;
