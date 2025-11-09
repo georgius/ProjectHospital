4849657a09c3c18aee2e61d58fd2c2ebb06fac4d
georgius <juraj.horvath.mail@gmail.com>
2025-11-09 14:46:34 +0100

    added procedure script switch state logging, added special reserved state for procedure script

---
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Lopital/ProcedureScriptPatch.cs
@@ -0,0 +1,33 @@
+﻿using GLib;
+using HarmonyLib;
+using Lopital;
+
+namespace ModAdvancedGameChanges.Lopital
+{
+    [HarmonyPatch(typeof(ProcedureScript))]
+    public static class ProcedureScriptPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ProcedureScript), nameof(ProcedureScript.SwitchState))]
+        public static bool SwitchStatePrefix(string state, ProcedureScript __instance)
+        {
+            if (!ViewSettingsPatch.m_enabled)
+            {
+                // Allow original method to run
+                return true;
+            }
+
+            if (__instance.m_stateData.m_state != state)
+            {
+                Entity entity = (__instance.m_stateData.m_procedureScene.m_patient != null) ? __instance.m_stateData.m_procedureScene.m_patient.GetEntity() : null;
+                entity = entity ?? __instance.m_stateData.m_procedureScene.EmployeeCharacter;
+
+                Debug.LogDebug(System.Reflection.MethodBase.GetCurrentMethod(), $"{entity?.Name ?? "NULL"}, script {__instance.m_stateData.m_scriptName}, switching state from {__instance.m_stateData.m_state} to {state}");
+            }
+
+            return true;
+        }
+
+        public const string STATE_RESERVED = "STATE_RESERVED";
+    }
+}
