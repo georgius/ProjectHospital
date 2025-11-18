aa54763f8d8cfb61c29c9ee5a9ee57d339a9a4c9
georgius <juraj.horvath.mail@gmail.com>
2025-11-18 10:22:20 +0100

    added examinations, fixed queuing by priority (select only first patient from queue)

---
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/Examinations.cs
@@ -0,0 +1,10 @@
+﻿namespace ModAdvancedGameChanges.Constants
+{
+    public static class Examinations
+    {
+        public static class Vanilla
+        {
+            public const string Reception = "EXM_RECEPTION_FAST";
+        }
+    }
+}
---
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Lopital/RoomPatch.cs
@@ -0,0 +1,61 @@
+﻿using GLib;
+using HarmonyLib;
+using Lopital;
+using System.Collections.Generic;
+
+namespace ModAdvancedGameChanges.Lopital
+{
+    [HarmonyPatch(typeof(Room))]
+    public static class RoomPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Room), nameof(Room.IsCharactersTurn))]
+        public static bool IsCharactersTurn(Entity character, Room __instance, ref bool __result)
+        {
+            if (!ViewSettingsPatch.m_enabled)
+            {
+                // Allow original method to run
+                return true;
+            }
+
+            if ((__instance.m_roomPersistentData.m_characterQueue.Count == 0) 
+                && (__instance.m_roomPersistentData.m_characterQueueLab.Count == 0) 
+                && (__instance.m_roomPersistentData.m_characterQueueUnits.Count == 0))
+            {
+                // all queues are empty
+                // character can continue
+
+                __result = true;
+                return false;
+            }
+
+            // check if character is on top of queue
+            if ((__instance.m_roomPersistentData.m_characterQueue.Count > 0) 
+                && (__instance.m_roomPersistentData.m_characterQueue[0] == character))
+            {
+                __result = true;
+                return false;
+            }
+
+            if ((__instance.m_roomPersistentData.m_characterQueueLab.Count > 0) 
+                && (__instance.m_roomPersistentData.m_characterQueueLab[0] == character))
+            {
+                __result = true;
+                return false;
+            }
+
+            if ((__instance.m_roomPersistentData.m_characterQueueUnits.Count > 0) 
+                && (__instance.m_roomPersistentData.m_characterQueueUnits[0] == character))
+            {
+                __result = true;
+                return false;
+            }
+
+            // default case
+            // character cannot continue
+
+            __result = false;
+            return false;
+        }
+    }
+}
