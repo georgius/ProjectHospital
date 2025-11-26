82610d2091b9ba7d45ee1b97628ebb2a167d2194
georgius <juraj.horvath.mail@gmail.com>
2025-11-26 07:32:59 +0100

    perks

--- ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/Perks.cs
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/Perks.cs
@@ -4,20 +4,22 @@
     {
         public static class Vanilla
         {
-            public const string FastLearner = "PERK_FAST_LEARNER";
-            public const string SlowLearner = "PERK_SLOW_LEARNER";
-            public const string LongCommute = "PERK_LONG_COMMUTE";
             public const string Alcoholism = "PERK_ALCOHOLISM";
             public const string Chemist = "PERK_CHEMIST";
-            public const string Scholar = "PERK_SCHOLAR";
+            public const string DiagnosticGenius = "PERK_DIAGNOSTIC_GENIUS";
+            public const string EarlyBird = "PERK_EARLY_BIRD";
+            public const string FastLearner = "PERK_FAST_LEARNER";
             public const string Gamer = "PERK_GAMER";
             public const string Germaphobe = "PERK_GERMAPHOBE";
-            public const string Hedonist = "PERK_HEDONIST";
-            public const string Spartan = "PERK_SPARTAN";
             public const string GoodBoss = "PERK_GOOD_BOSS";
+            public const string Hedonist = "PERK_HEDONIST";
+            public const string LongCommute = "PERK_LONG_COMMUTE";
             public const string NightOwl = "PERK_NIGHT_OWL";
-            public const string EarlyBird = "PERK_EARLY_BIRD";
             public const string PeoplePerson = "PERK_PEOPLE_PERSON";
+            public const string PracticalDiagnoses = "PERK_PRACTICAL_DIAGNOSES";
+            public const string Scholar = "PERK_SCHOLAR";
+            public const string SlowLearner = "PERK_SLOW_LEARNER";
+            public const string Spartan = "PERK_SPARTAN";
         }
     }
 }
