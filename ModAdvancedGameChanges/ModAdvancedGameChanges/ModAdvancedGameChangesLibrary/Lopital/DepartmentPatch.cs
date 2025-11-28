5a79f6a16a43d6a963fef1e19ec11799041274be
georgius <juraj.horvath.mail@gmail.com>
2025-11-28 19:31:15 +0100

    preserve average patient satisfaction

---
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Lopital/DepartmentPatch.cs
@@ -0,0 +1,43 @@
+﻿using GLib;
+using HarmonyLib;
+using Lopital;
+using ModAdvancedGameChanges.Constants;
+using System;
+using System.Linq;
+
+namespace ModAdvancedGameChanges.Lopital
+{
+    [HarmonyPatch(typeof(Department))]
+    public static class DepartmentPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Department), nameof(Department.GetAverageSatisfactionPatients))]
+        public static bool GetAverageSatisfactionPatientsPrefix(Department __instance, ref float __result)
+        {
+            if (!ViewSettingsPatch.m_enabled)
+            {
+                // Allow original method to run
+                return true;
+            }
+
+            int totalSatisfaction = 0;
+            int count = 0;
+
+            foreach (EntityIDPointer<Entity> entityIDPointer in __instance.m_departmentPersistentData.m_patients)
+            {
+                if (((!Tweakable.Vanilla.DlcHospitalServicesEnabled()) || __instance.m_departmentPersistentData.m_departmentType != Database.Instance.GetEntry<GameDBDepartment>(Departments.Vanilla.Pathology))
+                    && !entityIDPointer.GetEntity().GetComponent<BehaviorPatient>().IsHidden())
+                {
+                    totalSatisfaction += entityIDPointer.GetEntity().GetComponent<MoodComponent>().GetTotalSatisfaction();
+                    count++;
+                }
+            }
+
+            __result = (count == 0)
+                ? __instance.m_departmentPersistentData.m_todaysStatistics.m_averageSatisfactionPatients
+                : (float)totalSatisfaction / (100f * (float)count);
+
+            return false;
+        }
+    }
+}
