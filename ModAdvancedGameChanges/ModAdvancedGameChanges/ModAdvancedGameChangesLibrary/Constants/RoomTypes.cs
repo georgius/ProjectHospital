d8d9309d8e9f404f056f376c742350fba937a8f2
georgius <juraj.horvath.mail@gmail.com>
2025-12-09 20:26:22 +0100

    room types

--- ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/RoomTypes.cs
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/RoomTypes.cs
@@ -5,6 +5,7 @@
         public static class Vanilla
         {
             public const string CommonRoom = "ROOM_TYPE_COMMON_ROOM";
+            public const string Pharmacy = "DLC_ROOM_TYPE_PHARMACY";
             public const string Reception = "ROOM_TYPE_RECEPTION";
             public const string WaitingRoom = "ROOM_TYPE_WAITING";
             public const string WC = "ROOM_TYPE_WC";
