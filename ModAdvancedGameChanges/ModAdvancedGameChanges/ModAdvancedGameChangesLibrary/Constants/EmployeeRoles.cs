d88cbecf63166ac39a634d31c3c7ad2eb628c4fb
georgius <juraj.horvath.mail@gmail.com>
2025-12-09 20:26:46 +0100

    employee roles

--- ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/EmployeeRoles.cs
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Constants/EmployeeRoles.cs
@@ -5,6 +5,7 @@
         public static class Vanilla
         {
             public const string Diagnostics = "EMPL_ROLE_EXM_TRT";
+            public const string Pharmacist = "EMPL_ROLE_PHARMACIST";
             public const string Receptionist = "EMPL_ROLE_RECEPTIONIST";
         }
     }
