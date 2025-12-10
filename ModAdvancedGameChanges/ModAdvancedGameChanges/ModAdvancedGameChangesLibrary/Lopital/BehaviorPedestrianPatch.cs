7b82fab0c02fea084424a99aac7cb68898a0ee24
georgius <juraj.horvath.mail@gmail.com>
2025-12-10 21:40:11 +0100

    fix pedestrian access, reset pedestrian, randomly sending to phramacy

--- ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Lopital/BehaviorPedestrianPatch.cs
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Lopital/BehaviorPedestrianPatch.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using Lopital;
+using ModAdvancedGameChanges.Constants;
 using ModAdvancedGameChanges.Helpers;
 
 namespace ModAdvancedGameChanges.Lopital
@@ -7,6 +8,20 @@ namespace ModAdvancedGameChanges.Lopital
     [HarmonyPatch(typeof(BehaviorPedestrian))]
     public static class BehaviorPedestrianPatch
     {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(BehaviorPedestrian), nameof(BehaviorPedestrian.GetAccessRights))]
+        public static bool GetAccessRightsPrefix(BehaviorPedestrian __instance, ref AccessRights __result)
+        {
+            if ((!ViewSettingsPatch.m_enabled) || (!ViewSettingsPatch.m_enablePedestrianGoToPharmacy[SettingsManager.Instance.m_viewSettings].m_value))
+            {
+                // allow original method to run
+                return true;
+            }
+
+            __result = AccessRights.PATIENT;
+            return false;
+        }
+
         [HarmonyPrefix]
         [HarmonyPatch(typeof(BehaviorPedestrian), nameof(BehaviorPedestrian.SwitchState))]
         public static bool SwitchStatePrefix(BehaviorPedestrianState state, BehaviorPedestrian __instance)
@@ -24,6 +39,25 @@ namespace ModAdvancedGameChanges.Lopital
                 __instance.m_state.m_state = state;
                 __instance.m_state.m_timeInState = 0f;
             }
+
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(BehaviorPedestrian), "Reset")]
+        public static bool ResetPrefix(BehaviorPedestrian __instance)
+        {
+            if ((!ViewSettingsPatch.m_enabled) || (!ViewSettingsPatch.m_enablePedestrianGoToPharmacy[SettingsManager.Instance.m_viewSettings].m_value))
+            {
+                // allow original method to run
+                return true;
+            }
+
+            __instance.GetComponent<CharacterPersonalInfoComponent>().m_personalInfo = CharacterPersonalInfo.CreateRandomPersonalInfo(null, null);
+            __instance.GetComponent<AnimModelComponent>().Reset();
+
+            __instance.GetComponent<CharacterPersonalInfoComponent>().ChooseVoice(true);
+            __instance.m_entity.Name = "Pedestrian " + __instance.m_entity.GetComponent<CharacterPersonalInfoComponent>().m_personalInfo.GetFullName();
 
             return false;
         }
@@ -79,10 +113,33 @@ namespace ModAdvancedGameChanges.Lopital
                 return true;
             }
 
-            if (__instance.m_state.m_timeInState > __instance.m_state.m_respawnTimeSeconds)
+            if (!__instance.GetComponent<ProcedureComponent>().IsBusy())
             {
-                __instance.GetComponent<WalkComponent>().SetDestination(MapScriptInterface.Instance.DEBUG_GetPedestiranDestinationPosition(), 0, MovementType.WALKING);
-                __instance.SwitchState(BehaviorPedestrianState.Walking);
+                if (__instance.m_state.m_timeInState > __instance.m_state.m_respawnTimeSeconds)
+                {
+                    GameDBProcedure entry = Database.Instance.GetEntry<GameDBProcedure>(Procedures.Vanilla.Pharmacy);
+
+                    Department administrativeDepartment = MapScriptInterface.Instance.GetDepartmentOfType(Database.Instance.GetEntry<GameDBDepartment>(Departments.Vanilla.AdministrativeDepartment));
+                    ProcedureSceneAvailability availability = __instance.GetComponent<ProcedureComponent>().GetProcedureAvailabilty(entry, __instance.m_entity, administrativeDepartment, AccessRights.PATIENT, EquipmentListRules.ONLY_FREE);
+
+                    if ((UnityEngine.Random.Range(0f, 1f) <= Tweakable.Mod.PedestrianPharmacyProbability())
+                        && ((availability == ProcedureSceneAvailability.AVAILABLE)
+                            || (availability == ProcedureSceneAvailability.STAFF_BUSY)))
+                    {
+                        __instance.GetComponent<ProcedureComponent>().StartProcedure(entry, __instance.m_entity, administrativeDepartment, AccessRights.STAFF, EquipmentListRules.ONLY_FREE);
+                        __instance.SwitchState(BehaviorPedestrianState.Walking);
+                    }
+                    else if ((DayTime.Instance.GetDayTimeHours() > __instance.m_state.m_timeOfAppearance)
+                            && (DayTime.Instance.GetDayTimeHours() < 22f))
+                    {
+                        __instance.GetComponent<WalkComponent>().SetDestination(MapScriptInterface.Instance.DEBUG_GetPedestiranDestinationPosition(), 0, MovementType.WALKING);
+                        __instance.SwitchState(BehaviorPedestrianState.Walking);
+                    }
+                    else
+                    {
+                        __instance.m_state.m_timeInState = 0f;
+                    }
+                }
             }
 
             return false;
@@ -117,7 +174,7 @@ namespace ModAdvancedGameChanges.Lopital
                 return true;
             }
 
-            if (!__instance.GetComponent<WalkComponent>().IsBusy())
+            if ((!__instance.GetComponent<WalkComponent>().IsBusy()) && (!__instance.GetComponent<ProcedureComponent>().IsBusy()))
             {
                 __instance.Reset();
                 __instance.SwitchState(BehaviorPedestrianState.IdleAtDestination);
