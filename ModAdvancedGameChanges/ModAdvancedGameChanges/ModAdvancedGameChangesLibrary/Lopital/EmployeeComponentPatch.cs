450bac72f362947076cad53a1c890a85d1847f3a
georgius <juraj.horvath.mail@gmail.com>
2026-01-03 14:28:46 +0100

    updated constant

--- ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Lopital/EmployeeComponentPatch.cs
+++ ModAdvancedGameChanges/ModAdvancedGameChanges/ModAdvancedGameChangesLibrary/Lopital/EmployeeComponentPatch.cs
@@ -649,7 +649,7 @@ namespace ModAdvancedGameChanges .Lopital
             }
 
             // normalize satisfaction
-            __result = Mathf.Max(0f, Mathf.Min(100f, (float)__instance.m_entity.GetComponent<MoodComponent>().GetTotalSatisfaction())) / 100.0f;
+            __result = Mathf.Max(0f, Mathf.Min(100f, (float)__instance.m_entity.GetComponent<MoodComponent>().GetTotalSatisfaction())) / 200.0f;
             __result = UnityEngine.Random.Range(Tweakable.Mod.EfficiencySatisfactionMinimum(), Tweakable.Mod.EfficiencySatisfactionMaximum()) * __result + 50f;
 
             PerkComponent perkComponent = __instance.m_entity.GetComponent<PerkComponent>();
